02494ac02de31b7be5a6172f5afef85c8ddec8e9
qixianlu <qixian_lqx@163.com>
2018-07-27 12:41:30 +0800

    修改首页

---
+++ js/source/index.js
@@ -0,0 +1,21 @@
+function ViewModel() {
+    var self = this;
+    self.ProDetails = ko.observableArray(getProctions());
+}
+var vm = new ViewModel();
+ko.applyBindings(vm);
+
+//初始化产品数据
+function getProctions() {
+    var data = proList;
+    var prolist = [];
+    var count = 1;
+    $.each(data, function(index, obj) {
+
+        if (Enumerable.from(prolist).count("$.typeid==" + obj.typeid) < 4) {
+            prolist.push(obj);
+        }
+
+    });
+    return prolist;
+}
